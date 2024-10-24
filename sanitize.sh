fe188c7991428daeaa08e3142db40d7244acedff
Tre Landaiche <trebug123@gmail.com>
2024-10-23 21:26:04 -0500

    Better santize script

--- sanitize.sh
+++ sanitize.sh
@@ -1,26 +1,42 @@
+#!/bin/bash
+
 # Make an array of directories to sanitize
 dir_array=(./Good ./Bad ./Request)
 
-# Delete all non-gif files and gif files not starting with 'giphy' in each directory
+# Delete all non-gif files and, in folders ending with _files, delete gif files not starting with 'giphy'
 for dir in "${dir_array[@]}"; do
-    find "$dir" -type f \( ! -name "*.gif" -o \( -name "*.gif" ! -name "giphy*" \) \) -delete
+    # Delete all non-gif files
+    find "$dir" -type f ! -name "*.gif" -delete
+
+    # For directories ending with _files, delete gif files not starting with 'giphy'
+    find "$dir" -type d -name "*_files" | while read folder; do
+        find "$folder" -type f -name "*.gif" ! -name "giphy*" -delete
+    done
 done
 
-# Rename all remaining gif files to have the name of their directory
+# Rename all remaining gif files to have the name of their directory without the _files suffix
 for dir in "${dir_array[@]}"; do
-    # Get the base name of the directory
-    dir_name=$(basename "$dir")
-    
-    for file in "$dir"/*.gif; do
-        if [ -f "$file" ]; then  # Check if file exists
-            # Get the filename without the path
-            filename=$(basename "$file")
-            
-            # Remove "giphy" prefix and replace it with the directory name
-            new_name="${filename/giphy/$dir_name}"
-            
-            # Rename the file
-            mv "$file" "$dir/$new_name"
-        fi
+    find "$dir" -type d | while read folder; do
+        dir_name=$(basename "$folder")
+        dir_name_no_suffix=${dir_name%_files}
+        
+        for file in "$folder"/*.gif; do
+            if [ -f "$file" ]; then  # Check if file exists
+                # Get the filename without the path
+                filename=$(basename "$file")
+                
+                # Remove "giphy" prefix and replace it with the directory name without _files suffix
+                new_name="${filename/giphy/$dir_name_no_suffix}"
+                
+                # Rename the file
+                mv "$file" "$folder/$new_name"
+            fi
+        done
     done
+done
+
+# Rename folders ending with _files by removing the _files suffix
+find "${dir_array[@]}" -type d -name "*_files" | while read folder; do
+    new_folder_name=${folder%_files}
+    mv "$folder" "$new_folder_name"
 done
