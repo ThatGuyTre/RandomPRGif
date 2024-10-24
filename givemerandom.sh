df0068c48357f791d85f51f66e362a67275b8c12
Tre Landaiche <trebug123@gmail.com>
2024-10-23 21:15:43 -0500

    Added a bunch of new files for Request and edited code

--- givemerandom.sh
+++ givemerandom.sh
@@ -11,6 +11,9 @@ fi
 random_good_file=${good_files[RANDOM % ${#good_files[@]}]}
 cp "$random_good_file" "./Good.gif"
 
+# Print the results
+echo "Copied $random_good_file to ./Good.gif"
+
 # Find all GIF files in the Bad directory
 bad_files=(Bad/*/*.gif)
 
@@ -25,4 +28,21 @@ random_bad_file=${bad_files[RANDOM % ${#bad_files[@]}]}
 cp "$random_bad_file" "./Bad.gif"
 
 # Print the results
-echo "Copied $random_good_file to ./Good.gif and $random_bad_file to ./Bad.gif"
+echo "Copied $random_bad_file to ./Bad.gif"
+
+
+# Find all GIF files in the Request directory
+request_files=(Request/*/*.gif)
+
+# Check if there are any GIF files in Request
+if [ ${#request_files[@]} -eq 0 ]; then
+    echo "No GIF files found in the Request directory."
+    exit 1
+fi
+
+# Select a random GIF file from the Good directory
+random_request_file=${request_files[RANDOM % ${#request_files[@]}]}
+cp "$random_request_file" "./Request.gif"
+
+# Print the results
+echo "Copied $random_request_file to ./Request.gif"
---
+++ sanitize.sh
@@ -0,0 +1,26 @@
+# Make an array of directories to sanitize
+dir_array=(/Good /Bad /Request)
+
+# Delete all non-gif files and gif files not starting with 'giphy' in each directory
+for dir in "${dir_array[@]}"; do
+    find "$dir" -type f \( ! -name "*.gif" -o \( -name "*.gif" ! -name "giphy*" \) \) -delete
+done
+
+# Rename all remaining gif files to have the name of their directory
+for dir in "${dir_array[@]}"; do
+    # Get the base name of the directory
+    dir_name=$(basename "$dir")
+    
+    for file in "$dir"/*.gif; do
+        if [ -f "$file" ]; then  # Check if file exists
+            # Get the filename without the path
+            filename=$(basename "$file")
+            
+            # Remove "giphy" prefix and replace it with the directory name
+            new_name="${filename/giphy/$dir_name}"
+            
+            # Rename the file
+            mv "$file" "$dir/$new_name"
+        fi
+    done
+done
